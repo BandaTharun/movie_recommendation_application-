28b8f19c45746f50ff9f80e9d5d4eb37463a9bd5
BANDA THARUN REDDY <99380789+BandaTharun@users.noreply.github.com>
2024-05-22 19:12:24 +0200

    Update update_frontend_id.sh

--- code_files_to_update_k8s/update_frontend_id.sh
+++ code_files_to_update_k8s/update_frontend_id.sh
@@ -8,14 +8,14 @@ IMAGE_NAME="tharun9705/movie_recommendation_applications_repository"
 NEW_IMAGE="$IMAGE_NAME:$TAG"
 
 # Clone the repository
-git clone https://github.com/BandaTharun/movie_recommendation_application-.git
+git clone https://github.com/BandaTharun/movie_recommendation_application-.git /tmp/temp_repo
 
 # Navigate into the cloned repository directory
-cd movie_recommendation_application-/k8sfiles
+cd /tmp/temp_repo
 
 # Make changes to the Kubernetes manifest file(s)
 # Update the image tag in the frontend_k8s.yaml file
-sed -i "s|\(image: tharun9705/movie_recommendation_applications_repository:\).*|\1$TAG|" frontend_k8s.yaml
+sed -i "s|\(image: tharun9705/movie_recommendation_applications_repository:\).*|\1$TAG|" k8sfiles/frontend_k8's.yaml
 
 # Add the modified files
 git add frontend_k8s.yaml
@@ -29,4 +29,6 @@ git push
 # Cleanup: remove the temporary directory
 cd ../../
 rm -rf movie_recommendation_application-
+
+
 

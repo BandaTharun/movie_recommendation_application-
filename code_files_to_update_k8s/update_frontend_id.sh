929a79b3f0d6e680c1ebc9a9558ad0ebc4fa067f
BANDA THARUN REDDY <99380789+BandaTharun@users.noreply.github.com>
2024-05-22 19:37:41 +0200

    Update update_frontend_id.sh

--- code_files_to_update_k8s/update_frontend_id.sh
+++ code_files_to_update_k8s/update_frontend_id.sh
@@ -7,6 +7,11 @@ TAG=$1
 IMAGE_NAME="tharun9705/movie_recommendation_applications_repository"
 NEW_IMAGE="$IMAGE_NAME:$TAG"
 
+# Assuming "mrdir file" and "cd file" are placeholders for specific commands, I've commented them out
+# Uncomment and replace with actual commands if needed
+ mrdir file
+ cd file
+
 # Clone the repository
 git clone https://github.com/BandaTharun/movie_recommendation_application-.git /tmp/temp_repo
 
@@ -29,6 +34,5 @@ git push
 # Cleanup: remove the temporary directory
 cd /tmp
 rm -rf /tmp/temp_repo
-
 
 

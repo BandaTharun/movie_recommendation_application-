b8e829a7d77d6913aaa3b290aefe5ab652e85c5d
BANDA THARUN REDDY <99380789+BandaTharun@users.noreply.github.com>
2024-05-22 18:51:40 +0200

    Update update_frontend_id.sh

--- code_files_to_update_k8s/update_frontend_id.sh
+++ code_files_to_update_k8s/update_frontend_id.sh
@@ -2,25 +2,30 @@
 
 set -x
 
+# Input variable
+TAG=$1
+IMAGE_NAME="tharun9705/movie_recommendation_applications_repository"
+NEW_IMAGE="$IMAGE_NAME:$TAG"
+
 # Clone the repository
 git clone https://github.com/BandaTharun/movie_recommendation_application-.git
-
 
 # Navigate into the cloned repository directory
 cd movie_recommendation_application-/k8sfiles
 
 # Make changes to the Kubernetes manifest file(s)
-# For example, let's say you want to change the image tag in a deployment.yaml file
-sed -i "s|image:.*|image: <ACR-REGISTRY-NAME>/$1:$2|g" frontend_k8's.yaml
+# Update the image tag in the frontend_k8's.yaml file
+sed -i "s|\(image: tharun9705/movie_recommendation_applications_repository:\).*|\1$TAG|" frontend_k8's.yaml
 
 # Add the modified files
-git add .
+git add frontend_k8's.yaml
 
 # Commit the changes
-git commit -m "Update Kubernetes manifest"
+git commit -m "Update frontend image to $NEW_IMAGE"
 
 # Push the changes back to the repository
 git push
 
 # Cleanup: remove the temporary directory
+cd ../../
 rm -rf movie_recommendation_application-

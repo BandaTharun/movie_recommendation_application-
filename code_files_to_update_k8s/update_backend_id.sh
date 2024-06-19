078e8645cf6babb3f873e1d194b68f5c882afe96
BANDA THARUN REDDY <99380789+BandaTharun@users.noreply.github.com>
2024-06-19 21:55:35 +0200

    Update update_backend_id.sh

--- code_files_to_update_k8s/update_backend_id.sh
+++ code_files_to_update_k8s/update_backend_id.sh
@@ -1,17 +1,15 @@
+
+
 #!/bin/bash
 
 set -x
 
-
 # Input variables
-
 ACR_REGISTRY_NAME="tharun9705/movie_recommendation_applications_repository"
 TAG=$1
 YourGitHubToken=$2
 
 DEPLOYMENT_FILE_PATH="k8sfiles/k8sbackend/backend.yml"
-
-
 
 # Clone the repository using the PAT
 git clone https://${YourGitHubToken}@github.com/BandaTharun/movie_recommendation_application-.git /tmp/temp_repo
@@ -19,15 +17,16 @@ git clone https://${YourGitHubToken}@github.com/BandaTharun/movie_recommendation
 # Change directory to the cloned repo
 cd /tmp/temp_repo
 
-
 # Update the Kubernetes manifest
 sed -i "s|image:.*|image: $ACR_REGISTRY_NAME:$TAG|g" $DEPLOYMENT_FILE_PATH
 
 # Commit the changes
-git add .
+git add $DEPLOYMENT_FILE_PATH
 git commit -m 'Update Kubernetes manifest'
 
 # Push the changes
 git push https://${YourGitHubToken}@github.com/BandaTharun/movie_recommendation_application-.git
 
+# Cleanup
 rm -rf /tmp/temp_repo
+

225d290f88d57629fc64886bd8761bd41afa5bb7
BANDA THARUN REDDY <99380789+BandaTharun@users.noreply.github.com>
2024-06-20 02:10:46 +0200

    Update update_frontend_id.sh

--- code_files_to_update_k8s/update_frontend_id.sh
+++ code_files_to_update_k8s/update_frontend_id.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 # Input variables
 ACR_REGISTRY_NAME="tharun9705/movie_recommendation_applications_repository"
 TAG=$1
 YourGitHubToken=$2
 
-
 DEPLOYMENT_FILE_PATH="k8sfiles/k8sfrontend/frontend.yml"
+TEMP_REPO="/tmp/temp_repo"
 
+# Ensure /tmp/temp_repo directory is clean
+if [ -d "$TEMP_REPO" ]; then
+    rm -rf "$TEMP_REPO"
+fi
 
 # Clone the repository using the PAT
-git clone https://${YourGitHubToken}@github.com/BandaTharun/movie_recommendation_application-.git /tmp/temp_repo
+git clone https://${YourGitHubToken}@github.com/BandaTharun/movie_recommendation_application-.git "$TEMP_REPO"
 
 # Change directory to the cloned repo
-cd /tmp/temp_repo
+cd "$TEMP_REPO"
 
-# Update the Kubernetes manifest
-sed -i "s|image:.*|image: $ACR_REGISTRY_NAME:$TAG|g" $DEPLOYMENT_FILE_PATH
+# Update the Kubernetes manifest if the file exists
+if [ -f "$DEPLOYMENT_FILE_PATH" ]; then
+    sed -i "s|image:.*|image: $ACR_REGISTRY_NAME:$TAG|g" "$DEPLOYMENT_FILE_PATH"
+else
+    echo "Error: $DEPLOYMENT_FILE_PATH file not found!"
+    exit 1
+fi
 
 # Commit the changes
-git add $DEPLOYMENT_FILE_PATH
+git add "$DEPLOYMENT_FILE_PATH"
 git commit -m 'Update Kubernetes manifest'
 
 # Set git user configuration
@@ -32,7 +41,7 @@ git config user.email "youremail@example.com"
 git push https://${YourGitHubToken}@github.com/BandaTharun/movie_recommendation_application-.git
 
 # Cleanup
-rm -rf /tmp/temp_repo
+rm -rf "$TEMP_REPO"
 
 
 
